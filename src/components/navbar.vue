6527b582e2e5880a0ba90c70cf23bc7a0ccaca72
Rémi <Remi.patruno38@outlook.com>
2025-12-08 13:19:53 +0100

    1er commit de mis en ligne du premier exercice

---
+++ src/components/navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+        <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Nos Produits</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</template>
