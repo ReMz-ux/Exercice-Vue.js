fc958b21b6e1c79a4121352660c70f581cb02c5b
Rémi <Remi.patruno38@outlook.com>
2025-12-08 14:32:34 +0100

    Affichage depuis une fonction + opérations mathématiques + affichage avec une conditions ternaire

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import TextInterpolation from './components/TextInterpolation.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="" />
   <TextInterpolation />
   
 </template>
--- src/components/TextInterpolation.vue
+++ src/components/TextInterpolation.vue
@@ -1,8 +1,24 @@
 <template>
     <h1>Texte Interpolé</h1>
     <h2>{{ message }}</h2>
+    <p>{{ age }}</p>
+    <!--Affichage depuis une fonction-->
+    <h3>{{ SuperAffichage() }}</h3>
+    <!--Opérations mathématiques-->
+    <p>{{ 10+15 }}</p>
+    <p>{{ 999-250 }}</p>
+    <!--Exemple d'utilisation de conditions ternaire dans le template-->
+    <p class="ternaire">{{ age >= 18 ? 'Adulte' : 'Mineur' }}</p>
 </template>
 
 <script setup>
-const message = 'Bonjour, ceci est un texte interpolé dans Vue.js !';
+const message = 'Bien jouer tu a reussi a interpoler du texte !';
+const age = 25;
+
+function SuperAffichage() {
+    return 'Affichage depuis une fonction !';
+}
+
+
+
 </script>
